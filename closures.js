041dc016c424b24b0397458f2aa2d725ce378c77
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2024-01-10 10:47:03 +0600

    slove 3rd problem

--- closures.js
+++ closures.js
@@ -22,3 +22,23 @@ let counter = createCounter(10);
 console.log(counter()); //output : 10
 console.log(counter()); //output : 11
 console.log(counter()); // output : 12
+
+// 3. Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+// ** toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+// ** notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+
+const expect = function (val) {
+  const throwError = (errorStr) => {
+    throw new Error(errorStr);
+  };
+  return {
+    toBe: (val2) => val2 === val || throwError("Not Eequal"),
+    notToBe: (val2) => val2 !== val || throwError("Equal"),
+  };
+};
+
+// const e = expect(5).toBe(5);
+// const e2 = expect(5).notToBe(null);
+
+// console.log(e);
+// console.log(e2);
